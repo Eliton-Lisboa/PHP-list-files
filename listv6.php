e40d7437384d0aa62d97385783db21804438e449
Eliton Lisboa <76575902+Eliton-Lisboa@users.noreply.github.com>
2021-01-01 19:20:23 -0300

    Add files via upload

--- listv6.php
+++ listv6.php
@@ -6,17 +6,18 @@
     <?php
         $program_name = basename(__FILE__);
         $mode = isset( $_GET['mode'] ) ? $_GET['mode'] : 'light';
-        $dir = isset( $_GET['q'] ) ? $_GET['q'] : './';
-        $site = isset( $_GET['browser'] ) ? $_GET['browser'] : false;
+        $dir = isset( $_GET['search'] ) ? $_GET['search'] : './';
+        $browse = isset( $_GET['browser'] ) ? $_GET['browser']  : false;
         $removeex = isset( $_GET['removeex'] ) ? $_GET['removeex'] : false;
         $tmp = '';
+
         echo "
             <script>
                 let file_list = []
                 let folder_list = []
                 let mode = '$mode'
                 let program_name = '$program_name'
-                let dir
+                let dir = '$dir'
             </script>
         ";
     ?>
@@ -133,6 +134,7 @@
             border: 1px solid var(--form-border-color);
         }
 
+        /* Error */
         body div#initial div#error {
             display: none;
             margin-top: 50px;
@@ -149,15 +151,22 @@
             margin: 0px;
         }
 
+        /* Buttons */
         body div#initial div#buttons {
             padding-top: 8px;
         }
 
-        body div#initial div#buttons a {
+        body div#initial div#buttons a.item {
             text-decoration: none;
             outline: none;
             transition: .2s cubic-bezier(.3, 0, .5, 1);
+                -webkit-transition: .2s cubic-bezier(.3, 0, .5, 1);
+                -moz-transition: .2s cubic-bezier(.3, 0, .5, 1);
+                -o-transition: .2s cubic-bezier(.3, 0, .5, 1);
             user-select: none;
+                -webkit-user-select: none;
+                -moz-user-select: none;
+                -moz-user-select: none;
             display: block;
             width: 80%;
             text-align: center;
@@ -169,15 +178,15 @@
             background-color: var(--first-btn-background-color);
             border: 1px solid var(--first-btn-border-color);
         }
-        body div#initial div#buttons a:hover {
+        body div#initial div#buttons a.item:hover {
             color: var(--first-btn-hover-font-color);
             background-color: var(--first-btn-hover-background-color);
             border: 1px solid var(--first-btn-hover-border-color);
         }
-        body div#initial div#buttons a:active {
+        body div#initial div#buttons a.item:active {
             background-color: var(--first-btn-active-background-color);
         }
-        body div#initial div#buttons a:focus {
+        body div#initial div#buttons a.item:focus {
             /* box-shadow: var(--btn-focus-box-shadow); */
             /* border: 1px solid var(--first-btn-hover-border-color); */
             color: var(--first-btn-hover-font-color);
@@ -185,27 +194,26 @@
             border: 1px solid var(--first-btn-hover-border-color);
         }
 
-        body div#initial div#buttons a.btn-folder {
+        body div#initial div#buttons a.item.btn-folder {
             color: var(--second-btn-font-color);
             background-color: var(--second-btn-background-color);
             border: 1px solid var(--second-btn-border-color);
         }
-        body div#initial div#buttons a.btn-folder:hover {
+        body div#initial div#buttons a.item.btn-folder:hover {
             color: var(--second-btn-hover-font-color);
             border: 1px solid var(--second-btn-hover-border-color);
             background-color: var(--second-btn-hover-background-color);
         }
-        body div#initial div#buttons a.btn-folder:active {
+        body div#initial div#buttons a.item.btn-folder:active {
             background-color: var(--second-btn-active-background-color);
             color: var(--second-btn-hover-font-color);
         }
-        body div#initial div#buttons a.btn-folder:focus {
+        body div#initial div#buttons a.item.btn-folder:focus {
             color: var(--second-btn-hover-font-color);
             border: 1px solid var(--second-btn-hover-border-color);
             background-color: var(--second-btn-hover-background-color);
             
         }
-
 
         /* header */
         body div#initial div#header {
@@ -257,6 +265,7 @@
             border: 1px solid var(--second-btn-hover-border-color);
         }
 
+        /* Browser */
         body div#buttons {
             padding: 4px 0px 4px 0px;
         }
@@ -290,7 +299,7 @@
 <body id='body'>
 
     <?php
-        if ($site != false) {
+        if ($browse != false) {
             goto createBrowser;
         }
     ?>
@@ -310,10 +319,6 @@
         <div id='buttons'>
 
             <?php
-                echo "
-                <script>
-                    dir = '$dir'
-                </script>";
 
                 $directory = dir($dir);
                 while(($folder = $directory -> read()) != false)
@@ -321,7 +326,7 @@
                     if ( $folder != '.' and $folder != '..' and is_dir($dir. '/'. $folder) )
                     {
                         $tmp = strtolower(str_replace(' ', '-', $folder));
-                        echo "<a href='http://localhost:8080/$program_name?q=$dir/$folder&mode=$mode' class='item $tmp btn-folder'>$folder</a>";
+                        echo "<a href='http://localhost:8080/$program_name?search=$dir/$folder&mode=$mode' class='item $tmp btn-folder'>$folder</a>";
                         echo "
                         <script>
                             folder_list.push('$tmp')
@@ -336,7 +341,7 @@
                     {
                         $tmp = strtolower(str_replace(' ', '-', $file));
                         if ($removeex) { $file = substr($file, 0, -strlen(pathinfo($file, PATHINFO_EXTENSION)) - 1); }
-                        echo "<a href='http://localhost:8080/$program_name?mode=$mode&browser=http://localhost:8080/$dir/$file' class='item $tmp btn-file'>$file</a>";
+                        echo "<a href='http://localhost:8080/$program_name?mode=$mode&browser=http://localhost:8080/$dir/$file' class='item $tmp'>$file</a>";
                         echo "
                         <script>
                             file_list.push('$tmp')
@@ -344,7 +349,7 @@
                     }
                 }
                 $directory -> close();
-        
+
             ?>
 
         </div>
@@ -361,6 +366,7 @@
 
         <script>
             const $txbSearch = document.getElementById('txbSearch')
+            const $error     = document.getElementById('error'    )
             let tmp = ''
             let tmpbool
 
@@ -455,6 +461,10 @@
 
             })
 
+            if (file_list.length == 0 || folder_list.len == 0) {
+                $error.style.display = 'block'
+            }
+
         </script>
 
     </div>
@@ -462,20 +472,14 @@
     <?php
         createBrowser:
 
-        if ($site != false) {
+        if ($browse != false) {
             echo "
                 <div id='buttons'>
                     <svg class='btnBrowser' onclick='javascript:history.go(-1)' viewBox='0 0 512 512'> <path d='M492,236H68.442l70.164-69.824c7.829-7.792,7.859-20.455,0.067-28.284c-7.792-7.83-20.456-7.859-28.285-0.068l-104.504,104c-0.007,0.006-0.012,0.013-0.018,0.019c-7.809,7.792-7.834,20.496-0.002,28.314c0.007,0.006,0.012,0.013,0.018,0.019l104.504,104c7.828,7.79,20.492,7.763,28.285-0.068c7.792-7.829,7.762-20.492-0.067-28.284L68.442,276H492c11.046,0,20-8.954,20-20C512,244.954,503.046,236,492,236z'/> </svg>
                     <svg class='btnBrowser btnReload' onclick='window.location.reload()' viewBox='0 0 458.186 458.186'><g transform='matrix(-1,0,0,1,458.1860580444336,0)'><path d='M445.651,201.95c-1.485-9.308-10.235-15.649-19.543-14.164c-9.308,1.485-15.649,10.235-14.164,19.543c0.016,0.102,0.033,0.203,0.051,0.304c17.38,102.311-51.47,199.339-153.781,216.719c-102.311,17.38-199.339-51.47-216.719-153.781S92.966,71.232,195.276,53.852c62.919-10.688,126.962,11.29,170.059,58.361l-75.605,25.19c-8.944,2.976-13.781,12.638-10.806,21.582c0.001,0.002,0.002,0.005,0.003,0.007c2.976,8.944,12.638,13.781,21.582,10.806c0.003-0.001,0.005-0.002,0.007-0.002l102.4-34.133c6.972-2.322,11.675-8.847,11.674-16.196v-102.4C414.59,7.641,406.949,0,397.523,0s-17.067,7.641-17.067,17.067v62.344C292.564-4.185,153.545-0.702,69.949,87.19s-80.114,226.911,7.779,310.508s226.911,80.114,310.508-7.779C435.905,339.799,457.179,270.152,445.651,201.95z'/> </svg>
                 </div>
 
-                <iframe id='browser' src='$site' frameborder='0'></iframe>
-
-                <style>
-                    body {
-                        /* display: flex; */
-                    }
-                </style>
+                <iframe id='browser' src='$browse' frameborder='0'></iframe>
             ";
 
         }
